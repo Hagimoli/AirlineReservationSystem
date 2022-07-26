15bc4ddcf3c3d25ef2b8964f4be4178718929057
Hagimoli <hagimoli0123@gmail.com>
2022-07-26 23:44:45 +0700

    update
    update model

--- WebARS/Models/Booking.cs
+++ WebARS/Models/Booking.cs
@@ -7,5 +7,12 @@ namespace WebARS.Models
 {
     public class Booking
     {
+        public int Id { get; set; }
+        public int FlightId { get; set; }
+        public string Seat { get; set; }
+        public float Price { get; set; }
+        public int CustomerId { get; set; }
+        public Passenger Passenger { get; set; }
+        public Flight Flight { get; set; }
     }
 }
