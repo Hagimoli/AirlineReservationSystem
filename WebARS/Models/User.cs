087587a0d12080fc2c8549bdb7f9221243c94b06
Hagimoli <hagimoli0123@gmail.com>
2022-07-27 18:45:41 +0700

    update model

---
+++ WebARS/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebARS.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PaswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PhoneNumberConfirmed { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public string UserName { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
